5da2c1d5fcd0c4687e7d07968a5912d2ca74a115
LAPTOP-RBLOQQKD\Naswa Nida <naswanida3@gmail.com>
2025-03-03 08:51:36 +0700

    Praktikum 3, Pertanyaan No 4

--- MataKuliahDemo.java
+++ MataKuliahDemo.java
@@ -3,7 +3,10 @@ import java.util.Scanner;
 public class MataKuliahDemo {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        MataKuliah17[] arrayOfMataKuliah = new MataKuliah17[3];
+        System.out.print("Masukkan jumlah mata kuliah: ");
+        int jumlahMataKuliah = sc.nextInt();
+
+        MataKuliah17[] arrayOfMataKuliah = new MataKuliah17[jumlahMataKuliah];
 
         for (int i = 0; i < arrayOfMataKuliah.length; i++) {
             System.out.println("Masukkan Data Mata Kuliah ke-" + (i + 1));
